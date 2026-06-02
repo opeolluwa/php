be81412857f0017c2bdf39eac43ca0a7ca02feac
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-06-02 15:53:55 +0100

    initial commit

---
+++ multi.js
@@ -0,0 +1,18 @@
+
+let age = 7;
+age += 5;// age = age()7 + 5\
+
+
+let amount = 60
+amount *= 2; 
+
+
+let counter = 1;
+let sum = 0;
+
+
+while (counter < 150) {
+    sum += counter;
+    counter++;
+}
+console.log(sum);
